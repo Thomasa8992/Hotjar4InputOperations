a454db3fa1221eebd94304b38ad3d594b1b40be6
HEADQUARTERS\athomas1 <adrian.thomas@savvyotter.com>
2018-09-21 12:32:35 -0500

    create project that calculates a solution based on user input

---
+++ Hotjar4InputOperations/Hotjar4InputOperations/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotjar4InputOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool newQuery = true;
+            while (newQuery)
+            {
+                int value;
+                bool notValid = false; 
+                Console.WriteLine();
+                Console.WriteLine("Please enter first number: ");
+                int firstNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter operator: ");
+                string operators = Console.ReadLine();
+                Console.WriteLine("Please enter Second number: ");
+                int secondNumber = Convert.ToInt32(Console.ReadLine());
+                if (operators == "-")
+                {
+                    value = firstNumber - secondNumber;
+                    Console.WriteLine($"{firstNumber} {operators} {secondNumber} = " + value);
+                }
+                else if (operators == "/")
+                {
+                    value = firstNumber / secondNumber;
+                    Console.WriteLine($"{firstNumber} {operators} {secondNumber} = " + value);
+                }
+                else if (operators == "*" || operators == "x")
+                {
+                    value = firstNumber * secondNumber;
+                    Console.WriteLine($"{firstNumber} {operators} {secondNumber} = " + value);
+                }
+                else
+                {
+                    value = firstNumber + secondNumber;
+                    Console.WriteLine($"{firstNumber} {operators} {secondNumber} = " + value);
+                }
+                Console.WriteLine("Would you like to make another operation? If so, enter 'y' or 'yes', otherwise press ");
+                string decision = Console.ReadLine();
+
+                if (decision == "yes" || decision == "y")
+                {
+                    newQuery = true;
+                }
+                else
+                {
+                    newQuery = false;
+                }
+            } 
+        }
+    }
+}
